9ac12f83615ef44ad2ae9f3d4ccf4fe1062678ef
Martin Prokes <martin.prokes@indykite.com>
2023-03-30 13:57:03 +0200

    fix: adapt for vercel

--- server.js
+++ server.js
@@ -1,24 +1,56 @@
 const app = require('express')();
-const http = require('http').Server(app);
-const io = require('socket.io')(http);
+const server = require('http').createServer(app);
+const io = require('socket.io')(server);
 
-app.get('/', function(req, res){
-  res.sendFile(__dirname + '/index.html');
-});
+exports.handler = (event, context, callback) => {
+  const path = event.requestContext.resourcePath;
+  const method = event.requestContext.http.method;
+  const headers = event.headers;
+  const queryStringParameters = event.queryStringParameters;
+  const body = event.body ? JSON.parse(event.body) : {};
 
-io.on('connection', function(socket){
-  console.log('a user connected');
+  if (path === "/") {
+    if (method === "GET") {
+      // serve the chat UI
+      app.get('/', function(req, res){
+        res.sendFile(__dirname + '/index.html');
+      });
+    } else if (method === "POST") {
+      // handle chat messages
+      io.on('connection', function(socket){
+        console.log('a user connected');
 
-  socket.on('disconnect', function(){
-    console.log('user disconnected');
-  });
+        socket.on('disconnect', function(){
+          console.log('user disconnected');
+        });
 
-  socket.on('chat message', function(msg){
-    console.log('message: ' + msg);
-    io.emit('chat message', msg);
-  });
-});
+        socket.on('chat message', function(msg){
+          console.log('message: ' + msg);
+          io.emit('chat message', msg);
+        });
+      });
 
-http.listen(3000, function(){
-  console.log('listening on *:3000');
-});
+      // return a successful response
+      const response = {
+        statusCode: 200,
+        headers: {
+          'Content-Type': 'application/json',
+          'Access-Control-Allow-Origin': '*'
+        },
+        body: JSON.stringify({message: 'Chat messages are being handled.'})
+      };
+      callback(null, response);
+    }
+  }
+  
+  // handle unknown paths
+  const response = {
+    statusCode: 404,
+    headers: {
+      'Content-Type': 'application/json',
+      'Access-Control-Allow-Origin': '*'
+    },
+    body: JSON.stringify({message: 'Path not found.'})
+  };
+  callback(null, response);
+};
